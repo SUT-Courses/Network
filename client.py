907a6d72c7c738860774ca3593e97043f3ac6e9d
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-04-14 19:10:38 +0430

    client nearly done

---
+++ client.py
@@ -0,0 +1,24 @@
+import sys
+from socket import AF_INET, socket, SOCK_DGRAM
+
+from utils.logger import logger, get_timestr_mil_sec
+
+if len(sys.argv) != 3:
+    logger(msg="🔥Not acceptable🔥 use 'python client.py <host> <port>")
+    sys.exit(1)    
+
+_, host, port = sys.argv
+
+
+socket_client = socket(AF_INET, SOCK_DGRAM)
+socket_client.settimeout(2)
+
+for i in range(1,11):
+    msg = f"Ping {i} {get_timestr_mil_sec()}"
+    socket_client.sendto(msg.encode(), (host, int(port)))
+    try:
+        data, addr = socket_client.recvfrom(1024)
+        logger(msg=f"Received from server: {data.decode()}")
+    except:
+        logger(msg=f"Request timed out: {i}")
+
--- utils/logger.py
+++ utils/logger.py
@@ -4,7 +4,12 @@ def logger(msg: str):
     # print time and message
     print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]}] {msg}')
     
+def get_timestr_mil_sec() -> str:
+    # get milliseconds from datetime 
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
     
+    
+
 if __name__ == '__main__':
     # test
     logger('Hello World')
