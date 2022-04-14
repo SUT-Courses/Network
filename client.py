dc5ce17543bfcd1fc26034283c676db6a46dee98
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-04-14 20:33:40 +0430

    final commit

--- client.py
+++ client.py
@@ -6,21 +6,32 @@ from utils.logger import logger, get_timestr_mil_sec, check_command
 
 check_command(num_args=2, correct_command="'python client.py <port>'")    
 
+# server information
 host, port = "127.0.0.1", sys.argv[1]
 
-
+# init server
 socket_client = socket(AF_INET, SOCK_DGRAM)
 logger(f"Client started on port {port}")
+
+# init timeout
 socket_client.settimeout(2)
 
+# [1 .. 10]
 for i in range(1,11):
     msg = f"Ping {i} {get_timestr_mil_sec()}"
-    time.sleep(2)  
+    
+    # simulate some work in the client
+    time.sleep(1)  
+    
+    # send to server
     socket_client.sendto(msg.encode(), (host, int(port)))
     logger(msg=f"Sending from client: {msg}")
+
     try:
+        # received
         data, addr = socket_client.recvfrom(1024)
         logger(msg=f"Received from server: {data.decode()}")
     except timeout:
+        # timeout
         logger(msg=f"Request timed out: {i}")
 
--- server.py
+++ server.py
@@ -1,19 +1,29 @@
 import random
 import sys
+import time
 
 from socket import AF_INET, socket, SOCK_DGRAM
 from utils.logger import logger, get_timestr_mil_sec, check_command
 
 check_command(num_args=2, correct_command="'python server.py <port>'")
 port = int(sys.argv[1])
+
+# init server
 socket_server = socket(AF_INET, SOCK_DGRAM)
 socket_server.bind(('', port))
+
 logger(f"Server started on port {port}")
 
 while True:
+    # msg received from client
     msg, addr = socket_server.recvfrom(1024)
     logger(f"Received from client: {msg.decode()}")
+    
+    # perform actions on msg
     modified_msg = msg.decode().upper()
+    time.sleep(1)
+
+    # simulate packet loss
     if random.random() < 0.7:
         socket_server.sendto(modified_msg.encode(), addr)
         logger(f"Sending to client: {modified_msg}")
